dd648245519b2ce49629d547514bc663b7bfc11d
Shoaib Ahmed <shoaib.ahmed47564@gmail.com>
2020-03-31 10:00:53 +0500

    Devices Counted

--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import * as AWS from "aws-sdk";
+const keyID = "AKIAUNQAIZXDRSPKXDFJ";
+const keyPass = "01a9QRt20YrixEz1pRzqdnoGN8Od1AGHIChB5SR4";
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "dynamodb.us-west-2.amazonaws.com",
+  accessKeyId: keyID,
+  secretAccessKey: keyPass
+});
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
-}
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
 
 class App extends Component {
+  dynamodb = new AWS.DynamoDB();
+  docClient = new AWS.DynamoDB.DocumentClient();
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfDevicesDetected: null
+    };
+  }
+  onRead = () => {
+    let params = {
+      TableName: "kkm_002TableA"
+    };
+
+    this.docClient.scan(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(data.Items[0].Data.obj);
+        var countedIds = [];
+        var numberOfDevicesDetected = 0;
+        data.Items[0].Data.obj.forEach(async element => {
+          if (element["minorID"] != null) {
+            var found = false;
+            await countedIds.map(id => {
+              if (id === element["minorID"]) found = true;
+            });
+            if (!found) {
+              numberOfDevicesDetected++;
+              countedIds.push(element["minorID"]);
+            }
+            this.setState({ numberOfDevicesDetected });
+          }
+        });
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.onRead();
+  }
   render() {
+    const { numberOfDevicesDetected } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Clock date={new Date()} />
+          <div>
+            <h3>Number of Devices Detected: {numberOfDevicesDetected}</h3>
+          </div>
         </header>
       </div>
     );
